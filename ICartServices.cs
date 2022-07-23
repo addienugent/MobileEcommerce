e09d1797d8174f095307bf3e75254e60360a0f04
addienugent <102487019+addienugent@users.noreply.github.com>
2022-07-22 20:57:41 -0400

    Update ICartServices.cs

--- ICartServices.cs
+++ ICartServices.cs
@@ -1,1 +1,24 @@
 
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Library.MobileEcommerce
+{
+	public interface ICartServices
+  {
+        public List<CartService> SavedCartsList { get; }
+
+        ObservableCollection<Item> GetCartProds();
+        void SaveCart(ObservableCollection<Item> carts);
+
+        void AddToCart(Product myprod);
+
+        void RemoveFromCart(int id);
+
+        void Clear();
+        }
+}
