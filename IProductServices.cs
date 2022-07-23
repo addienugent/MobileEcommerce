00793ab14f44fcdcde310e90ae0a62ab1d09942c
addienugent <102487019+addienugent@users.noreply.github.com>
2022-07-22 21:08:16 -0400

    Update IProductServices.cs

--- IProductServices.cs
+++ IProductServices.cs
@@ -1,1 +1,13 @@
+using System.Collections.Generic;
+using XamarinCapstoneProj.Models;
 
+namespace MobileEcommerce
+{
+	public interface IProductServices : IService<Products>
+	{
+		List<Products> GetSortedAll(int Id);
+		string Add();
+    
+		string ProdCount();
+	}
+}
